56296ce6f2eb085af56921e1e02b671f062effbc
Flavio <flavio.simonelli.se@gmail.com>
2026-01-26 17:35:40 +0100

    add Randoop Test ProxyManagerImpl

---
+++ openjpa-kernel/src/test-randoop/java/org/apache/openjpa/util/ProxyManagerImplRandoopTest.java
@@ -0,0 +1,8 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ ProxyManagerImplRandoopTest0.class, ProxyManagerImplRandoopTest1.class, ProxyManagerImplRandoopTest2.class })
+public class ProxyManagerImplRandoopTest {
+}
+
